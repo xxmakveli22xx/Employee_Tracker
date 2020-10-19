988da5354d3113163607f652b657b292f52fd219
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-10-18 19:19:01 -0500

    added env file

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost", 
     user: "root",
-    password: "Ryker$12",
+    password: NONE,
     database: "employees" 
 
     
